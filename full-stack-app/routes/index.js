eef9798b5bcfee9916eff004c259c4534f03b61e
RhysG <rhys.gillham.101@outlook.com.au>
2025-05-14 11:02:35 +0800

    Minor cleanup for deployment

--- full-stack-app/routes/index.js
+++ full-stack-app/routes/index.js
@@ -6,11 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', header: 'Index page', description: 'This is the index page of the application.' });
 });
 
-/* Variation home page. */
-router.get('/test', function(req, res, next) {
-  res.render('index', { title: 'This is a test page' });
-});
-
-
 
 module.exports = router;
